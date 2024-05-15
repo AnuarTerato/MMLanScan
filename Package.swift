08989f9b7be2a92b77164eda88d59b9890737a50
Anuar Nordin <anuarnordin22081991@gmail.com>
2024-05-15 13:29:11 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,11 @@ import PackageDescription
 let package = Package(
     name: "MMLanScanner",
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MMLanScanner",
-            path: "Sources")
+            dependencies: ["FASwiftUI"]
+        )
     ]
 )
