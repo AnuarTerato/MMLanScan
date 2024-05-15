fc58f3c3850c6e019237eb4f3a975a01bb5faed8
Anuar Nordin <anuarnordin22081991@gmail.com>
2024-05-15 12:45:02 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,21 @@
 // swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// import PackageDescription
+
+// let package = Package(
+//     name: "MMLanScanner",
+//     targets: [
+//         .target(
+//             name: "MMLanScanner",
+//             path: "Sources")
+//     ]
+// )
+
 import PackageDescription
 
 let package = Package(
-    name: "MMLanScanner",
-    targets: [
-        .target(
-            name: "MMLanScanner",
-            path: "Sources")
-    ]
-)
+  name: "MMLanScanner",
+  platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
+  products: [.library(name: "MMLanScanner", targets: ["MMLanScanner"])],
+  targets: [.target(name: "MMLanScanner", path: "Sources")])
