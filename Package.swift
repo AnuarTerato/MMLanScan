e79f97b1c39f9ce97e7c5218559d9f44cdacd743
Anuar Nordin <anuarnordin22081991@gmail.com>
2024-05-15 11:51:28 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,17 +4,17 @@
 import PackageDescription
  
 let package = Package(
-    name: "MMLanScanner",
+    name: "LanScanner",
     products: [
         .library(
-            name: "MMLanScanner",
-            targets: ["MMLanScanner"])
+            name: "LanScanner",
+            targets: ["LanScanner"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MMLanScanner",
+            name: "LanScanner",
             dependencies: [])
     ]
 )
