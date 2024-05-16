49ab49a34e8b4c4ca16ed4afe5051c0f85e8fada
Anuar Nordin <anuarnordin22081991@gmail.com>
2024-05-16 19:44:46 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,5 +4,4 @@ import PackageDescription
 let package = Package(
   name: "MMLanScanner",
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
-  products: [.library(name: "MMLanScanner")],
   targets: [.target(name: "MMLanScanner", path: "Sources")])
