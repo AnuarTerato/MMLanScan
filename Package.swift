06f2868faad070f905a45526c6d751ff00fab965
Anuar Nordin <anuarnordin22081991@gmail.com>
2024-05-16 19:15:07 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,12 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MMLanScanner",
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MMLanScanner",
-            path: "Sources"
-        )
+    platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
+    targets: [.target(name: "MMLanScanner", path: "Sources")
     ]
 )
