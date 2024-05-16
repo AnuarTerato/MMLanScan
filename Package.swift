cdf7dbfefd548aca30557a0855206134f6f1d5cd
Anuar Nordin <anuarnordin22081991@gmail.com>
2024-05-16 18:37:06 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MMLanScanner",
-            dependencies: ["FASwiftUI"]
+            dependencies: ["FASwiftUI"],
+            path: "Sources"
         )
     ]
 )
