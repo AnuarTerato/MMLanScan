0c5b88f9ab95b4857efb1e0253285361f348a958
Anuar Nordin <anuarnordin22081991@gmail.com>
2024-05-16 18:58:23 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MMLanScanner",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "MMLanScanner"),
+        .library(name: "MMLanScanner")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
